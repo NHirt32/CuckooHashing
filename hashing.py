9b7668755f03f6dc357673db5dffd0aba2de3718
NHirt32 <nicholas.t.hirt@gmail.com>
2023-11-06 08:14:17 -0800

    Commented variables to relate them to research paper's names. Worked on basic hashInsert, however, it is untested yet. Need to get access to [26] to find what 1 + epsilon is.

--- hashing.py
+++ hashing.py
@@ -1,23 +1,33 @@
 import random
 
 # rehash is the function that manually doubles the size of our tables
-def rehash(currSize, table1, table2):
+def rehash(currSize):
     currSize = currSize * 2;
     # Manually we are doubling the size of our tables and setting their lengths with 'None' values
     table1 = [None] * currSize
     table2 = [None] * currSize
-    return (table1,table2, currSize);
+    return (table1, table2);
 
-def hashInsert(table1, table2, key, index, loopCount):
-    if(table1[index] == [None]):
-        table1[index] = key;
+# hashInsert parameters are our two tables, our key value, the hashed index of our key, and the current number of
+# iterations in the current attempted insert
+def hashInsert(table1, table2, key, index, loopCount, maxLoop):
+    loopCount += 1;
+    if(maxLoop != loopCount):
+        if(table1[index] == [None]):    # If the index is empty from a rehash or from initial creation we can simply insert the value
+            table1[index] = key;
+        else:
+            hashInsert(table2, table1, table1[index], index, loopCount, maxLoop);  # If the index isn't empty we recursively call hashInsert again swapping the table we are inserting to as well as the value we are trying to insert
+            table1[index] = key;    # If we have not reached maxLoop we can then travel back through the stack setting our new index values accordingly
+        return (table1, table2);
     else:
-        loopCount += 1;
+        newData = rehash(len(table1));
+        table1 = newData[0];
+        table2 = newData[1];
+    return (table1, table2);
 
-    return;
 
 
-
+# hashInfo is a class we will make objects with that will contains h1 and h2 for each hashing instance of our data structure
 class hashInfo:
     randomVar1 = random.randint(1,1000000); # Temporarily bounding random values 1-1m
     randomVar2 = random.randint(1,1000000);
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import random
 
 import hashing
@@ -6,19 +7,19 @@ import hashing
 lowerBound = 1;
 upperBound = 100;
 sampleAmount = 5;
-dataSet = random.sample(range(lowerBound,upperBound), sampleAmount);
+dataSet = random.sample(range(lowerBound,upperBound), sampleAmount); # From the paper we should have r^2 keys
 currSize = 10;
+maxLoop = 3 * math.log(len(dataSet), base); # Base should be 1 + epsilon, can't access resource tho...
 
-
-table1 = [0];
+table1 = [0];   # From the paper both table1, and table2 are of length 'r'
 table2 = [0];
 
 # Testing rehash
-newData = hashing.rehash(currSize, table1, table2);
+newData = hashing.rehash(currSize);
 # Setting new values post rehash
 table1 = newData[0];
 table2 = newData[1];
-currSize = newData[2];
+
 print(f'Table Data post rehash(): \nLength of tables: {currSize}');
 
 table1Hash = hashing.hashInfo();
