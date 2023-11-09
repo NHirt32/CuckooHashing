b33a421a9dade786b6c2c1e13335e2428c14811f
NotDesp <nicholas.t.hirt@gmail.com>
2023-11-09 08:50:47 -0800

    This is the version first presented to Colton. Rehash still not really functional and the program breaks (or doesn't do anything? idk) upon hitting maxLoop. Currently using base e log as do not have access to 1 + epsilon resource.

--- hashing.py
+++ hashing.py
@@ -1,4 +1,7 @@
 import random
+import math
+
+largePrime = 7919;
 
 # rehash is the function that manually doubles the size of our tables
 def rehash(currSize):
@@ -6,24 +9,54 @@ def rehash(currSize):
     # Manually we are doubling the size of our tables and setting their lengths with 'None' values
     table1 = [None] * currSize
     table2 = [None] * currSize
-    return (table1, table2);
+    hash1 = hashInfo();
+    hash2 = hashInfo();
+    return (table1, table2, hash1, hash2);
 
 # hashInsert parameters are our two tables, our key value, the hashed index of our key, and the current number of
 # iterations in the current attempted insert
-def hashInsert(table1, table2, key, index, loopCount, maxLoop):
+def hashInsert(table1, table2, hash1, hash2, key, loopCount, maxLoop):
     loopCount += 1;
-    if(maxLoop != loopCount):
-        if(table1[index] == [None]):    # If the index is empty from a rehash or from initial creation we can simply insert the value
-            table1[index] = key;
+    tableStatePrint(table1, table2);
+    index = hash1.hFunc(key, len(table1), largePrime);
+    if(maxLoop > loopCount):   # If we haven't reached the maximum number of attempted inserts...
+        print(f'Insertion Info: Table1 Index = {index}, Key Value = {key}, Current Loop = {loopCount}, maxLoop = {maxLoop}');
+        if table1[index] is None:    # If the index is empty from a rehash or from initial creation we can simply insert the value
+            table1[index] = key;    # Set the value of the index = to our key
         else:
-            hashInsert(table2, table1, table1[index], index, loopCount, maxLoop);  # If the index isn't empty we recursively call hashInsert again swapping the table we are inserting to as well as the value we are trying to insert
+            hashInsert(table2, table1, hash2, hash1, table1[index], loopCount, maxLoop);  # If the index isn't empty we recursively call hashInsert again swapping the table we are inserting to as well as the value we are trying to insert
             table1[index] = key;    # If we have not reached maxLoop we can then travel back through the stack setting our new index values accordingly
         return (table1, table2);
-    else:
+    else:   # If we have reached the maximum number of attempted inserts...
+        print("MAX LOOP HAS BEEN REACHED REHASHING")
         newData = rehash(len(table1));
         table1 = newData[0];
         table2 = newData[1];
+        hash1 = newData[2];
+        hash2 = newData[3];
     return (table1, table2);
+
+def cuckooHash():
+    # Dummy data for testing
+    lowerBound = 1;
+    upperBound = 102;
+    currSize = 10;  # This is 'r' from the paper
+    sampleAmount = 2 * currSize;  # This is r^2 from the paper, temporarily 2r since idk whats happening...
+    dataSet = random.sample(range(lowerBound, upperBound), sampleAmount);  # Random sample of keys
+    maxLoop = 3 * math.log(len(dataSet), 2);  # Base should be 1 + epsilon, can't access resource tho...
+
+    table1 = [None] * currSize;  # From the paper both table1, and table2 are of length 'r'
+    table2 = [None] * currSize;
+
+    hash1 = hashInfo();
+    hash2 = hashInfo();
+
+    print(f'Our data set: {*dataSet,}')
+
+    for key in dataSet:
+        newValues = hashInsert(table1, table2, hash1, hash2, key, 0, maxLoop);
+        table1 = newValues[0];
+        table2 = newValues[1];
 
 
 
@@ -32,6 +65,13 @@ class hashInfo:
     randomVar1 = random.randint(1,1000000); # Temporarily bounding random values 1-1m
     randomVar2 = random.randint(1,1000000);
     def hFunc(self, key, listLength, primeValue):
+        # print(f'key = {key}, listLength = {listLength}');
         hashedAddress = ((self.randomVar1 * key + self.randomVar2) % primeValue) % listLength;
-        return(key, hashedAddress);
+        return hashedAddress;
 
+
+# Debug printing
+def tableStatePrint(table1, table2):
+    print("---------------------------------------")    # Formatting
+    print(f'Table 1: {*table1,}');
+    print(f'Table 2: {*table2,}')
--- main.py
+++ main.py
@@ -1,37 +1,3 @@
-import math
-import random
-
 import hashing
 
-# Dummy data for testing
-lowerBound = 1;
-upperBound = 100;
-sampleAmount = 5;
-dataSet = random.sample(range(lowerBound,upperBound), sampleAmount); # From the paper we should have r^2 keys
-currSize = 10;
-maxLoop = 3 * math.log(len(dataSet), base); # Base should be 1 + epsilon, can't access resource tho...
-
-table1 = [0];   # From the paper both table1, and table2 are of length 'r'
-table2 = [0];
-
-# Testing rehash
-newData = hashing.rehash(currSize);
-# Setting new values post rehash
-table1 = newData[0];
-table2 = newData[1];
-
-print(f'Table Data post rehash(): \nLength of tables: {currSize}');
-
-table1Hash = hashing.hashInfo();
-
-hashVal = table1Hash.hFunc(dataSet[0], len(table1), 859);
-print(hashVal);
-
-# Before trying hashInsert
-loopCount = 0;
-
-# Practice printing
-# print(table1);
-# print(table2);
-# print(currSize);
-# print(testHash[1]);
+hashing.cuckooHash();
